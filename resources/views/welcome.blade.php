85f8501fb9c2df24eec145f411a1ebe703589806
Raja <kraja200709@gmail.com>
2021-04-23 08:59:43 +0530

    favicons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Annamayya Urban Development Authority</title>
+
+        {{-- favicon --}}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/favicons/site.webmanifest">
+
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -100,7 +107,7 @@
                         </div>
                     </div>
                 </div>
-
+   <img src="/favicons/favicon-32x32.png"/>
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
@@ -128,5 +135,7 @@
                 </div>
             </div>
         </div>
+
+     
     </body>
 </html>
